b14e164b978ba4816b710c702d7feb007942abd2
Rayaan Grewal <rayaangrewal@gmail.com>
2020-08-03 12:46:23 +0530

    bug fixes

---
+++ build/precache-manifest.395f8fa0f1b9afda5fa14fc78df8d9c5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "040e82716a28b9c8284b65c98e70d091",
+    "url": "/index.html"
+  },
+  {
+    "revision": "985c94a4cb4115ecaf48",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "265f48b49ef444a23d43",
+    "url": "/static/css/main.950553f2.chunk.css"
+  },
+  {
+    "revision": "985c94a4cb4115ecaf48",
+    "url": "/static/js/2.08440ed7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.08440ed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265f48b49ef444a23d43",
+    "url": "/static/js/main.d07d38fb.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "1847ab3e2f436cc5589902d8fe063bb7",
+    "url": "/static/media/burger.1847ab3e.jpeg"
+  },
+  {
+    "revision": "1a4b784269ee2bb45059d3465825fa97",
+    "url": "/static/media/loading.1a4b7842.svg"
+  },
+  {
+    "revision": "646e88726e515f785786966798b38977",
+    "url": "/static/media/pizza.646e8872.jpg"
+  },
+  {
+    "revision": "af5cb8b14a7e3a277009b2c2432dd857",
+    "url": "/static/media/salad.af5cb8b1.jpg"
+  }
+]);
